fabde970ec5c6c3a1592e11e97e4304c73be9064
yeray <yeray.programacion.web@gmail.com>
2024-06-26 17:39:35 +0100

    kata working

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
-export function myFunction() {
-  return true
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+let nombre = process.argv[2];
+console.log(`Buenos dias ${nombre}`)
+
+while (true) {
+  const answer = await readLine();
+  if(answer === "Stop!") {
+    break
+  }
+  let reversed = answer.split("").reverse().join("");
+  console.log(reversed)
+} 
+
+async function readLine() {
+  const rl = readline.createInterface({ input, output });
+  const answer = await rl.question('');
+  rl.close();
+
+  return answer;
 }
+
+
+
+
